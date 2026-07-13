3c459bf30139623a988909352f79a297b996e100
Alfredo Palafox <ingalfredopalafox@gmail.com>
2026-07-13 13:55:46 -0600

    Primer commit: Simulador de sensores e IP API listo para la nube

---
+++ server.js
@@ -0,0 +1,15 @@
+{
+  "name": "grownet-simulador-api",
+  "version": "1.0.0",
+  "description": "Simulador de sensores e IP API para GrowNet",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "json-server": "^0.17.4"
+  },
+  "engines": {
+    "node": ">=18.0.0"
+  }
+}
