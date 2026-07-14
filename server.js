76710819804de80d18b10f70e95aec0493687b9b
Alfredo Palafox <ingalfredopalafox@gmail.com>
2026-07-13 23:44:48 -0600

    Refactor: Cambiar llave lng por lon en el endpoint de sensores

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const server = jsonServer.create();
 const middlewares = jsonServer.defaults();
 
 // Códigos de clima oficiales (WMO): 0=Despejado, 3=Nublado, 80=Lluvia
-const codigosClima =; 
+const codigosClima = [0, 1, 2, 3, 45, 48, 51, 53, 55, 56, 57, 61, 63, 65, 66, 67, 71, 73, 75, 77, 80, 81, 82, 85, 86];
 
 // Banco de ubicaciones compartidas (Usa "lon" de forma unificada)
 const catalogoUbicaciones = [
